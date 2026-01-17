2ec430dbcb0745899f747151e656cf846557872d
ferhatozdemirr <117825221+ferhatozdemirr@users.noreply.github.com>
2026-01-18 02:28:26 +0300

    unused codes has been deleted.

--- utils/textProps.ts
+++ utils/textProps.ts
@@ -9,6 +9,3 @@ export const TEXTS = {
   pushToSelectedText : "Seçilenleri Gönder",
   succesText : "Başarıyla gönderildi.",
 }
-
-export const FREQUENCY_AXIS = [1, 2, 3, 4, 5];
-export const MONETARY_AXIS = [5, 4, 3, 2, 1];
